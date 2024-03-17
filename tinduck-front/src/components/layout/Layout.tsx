9e69da02f9d82ea8a1f5424cf5eb53b04f1ba925
y.golev <y.golev@runetsoft.ru>
2024-03-17 15:27:05 +0300

    refactoring directory

--- tinduck-front/src/components/layout/Layout.tsx
+++ tinduck-front/src/components/layout/Layout.tsx
@@ -1,23 +1,19 @@
 import React from "react"
 import {Navigate, Outlet} from "react-router-dom"
-import {FooterNav} from "./FooterNav.tsx"
+import {FooterNav} from "./components/FooterNav.tsx"
 import {useAuth} from "../../hooks/useAuth.tsx"
-import {Header} from "./Header.tsx"
+import {Header} from "./components/Header.tsx"
 
 export const Layout: React.FC = () => {
   const isAuth: boolean = useAuth()
 
   return isAuth ? (
     <div className="flex flex-col h-full w-full max-w-[768px] mx-auto container-app">
-      <div className="z-20 w-full fixed top-[0px] container-app">
-        <Header></Header>
-      </div>
+      <Header></Header>
       <div className="container-page flex-1 flex flex-col h-full">
         <Outlet></Outlet>
       </div>
-      <div className="z-20 w-full fixed bottom-[0] container-app">
-        <FooterNav></FooterNav>
-      </div>
+      <FooterNav></FooterNav>
     </div>
   ) : (
     <Navigate to={"/login"}></Navigate>
