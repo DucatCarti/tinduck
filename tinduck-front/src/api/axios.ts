b12dd72e8c60965224ea0a7ad1831f6339ac26d6
y.golev <y.golev@runetsoft.ru>
2024-03-18 21:27:57 +0300

    fix axios configure

--- tinduck-front/src/api/axios.ts
+++ tinduck-front/src/api/axios.ts
@@ -3,32 +3,11 @@ import axios from "axios"
 const BASE_PORT = 3001
 export const BASE_URL = `http://192.168.0.104:${BASE_PORT}`
 
-export const authAxios = axios.create({
-  baseURL: `${BASE_URL}/api/auth/`,
+export const $axios = axios.create({
+  baseURL: `${BASE_URL}/api/`,
   headers: {
     "Content-Type": "application/json",
   },
-})
-export const $axios = axios.create({
-  baseURL: `${BASE_URL}/api/`,
-})
-authAxios.interceptors.response.use(
-  (response) => {
-    return response
-  },
-  (error) => {
-    if (error.response.status === 401 || error.response.status === 403) {
-      if (localStorage.getItem("token")) {
-        localStorage.removeItem("token")
-        delete authAxios.defaults.headers.common["Authorization"]
-      }
-    }
-    return error.response
-  },
-)
-authAxios.interceptors.request.use((config) => {
-  config.headers.Authorization = localStorage.getItem("token")
-  return config
 })
 $axios.interceptors.response.use(
   (response) => {
@@ -38,7 +17,7 @@ $axios.interceptors.response.use(
     if (error.response.status === 401 || error.response.status === 403) {
       if (localStorage.getItem("token")) {
         localStorage.removeItem("token")
-        delete authAxios.defaults.headers.common["Authorization"]
+        delete $axios.defaults.headers.common["Authorization"]
       }
     }
     return error.response
